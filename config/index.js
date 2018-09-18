fbdd7242ba2bdcec7f5066a42fab46daf71bce36
569589854 <sun569589854@163.com>
2018-09-18 21:48:42 +0800

    master

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     proxyTable: {
       // 获取推荐歌单
       '/api/getDiscList': {
