05f3a81e0d9f31d5652b138b1c7862aef4dfe4dd
569589854 <sun569589854@163.com>
2018-09-18 21:55:18 +0800

    done

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
     proxyTable: {
       // 获取推荐歌单
       '/api/getDiscList': {
@@ -151,7 +151,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
